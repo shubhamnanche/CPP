8e4777c3c437583bc450fd5d5e07d64f3b73bc8d
Shubham Nanche <73213552+shunax19@users.noreply.github.com>
2022-01-15 16:56:08 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -5,11 +5,12 @@ int main() {
     int n;
     cin >> n;
     int arr[n];
+    // taking the elements of the array from the user
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
     
-    //selection sort
+    //selection sort algorithm
     for(int i=0; i<n-1; i++){
         for(int j=i+1; j<n; j++){
             if(arr[j]<arr[i]){
@@ -21,14 +22,15 @@ int main() {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-                cout << " i >> " << i << "; j >> " << j << " ; " << arr[i] << " " << arr[j] << endl;
+                // displaying the current result
+                //cout << " i >> " << i << "; j >> " << j << " ; " << arr[i] << " " << arr[j] << endl;
             }
         }
     }
 
-    cout << endl;
+    cout << "The sorted array (using selection sort) is : "<< endl;
     for(int i=0; i<n; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << " ";
     }
     return 0;
 }
